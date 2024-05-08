1799e2263e651ce1949471a38a0d5b315bf0f1c8
poppopp2 <166694558+poppopp2@users.noreply.github.com>
2024-05-08 17:47:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import streamlit as st
 from corr import corr
 from prediction import prediction
+import matplotlib.pyplot as plt
 
 
 def main():
+    import platform
+    from matplotlib import font_manager, rc
+    plt.rcParams['axes.unicode_minus'] = False
+    if platform.system() == 'Linux':
+        rc('font', family='NanumGothic')
+
     st.markdown('출처 : https://ecos.bok.or.kr/#/ ')
     choice=st.sidebar.selectbox('옵션', options=['상관계수','코스피 가격 예측'])
 
